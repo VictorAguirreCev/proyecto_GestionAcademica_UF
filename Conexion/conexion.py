0a6cb944bebd209ebcb93e655af8140f1212ab95
VictorAguirreCev <vb.aguirrec@uea.edu.ec>
2026-03-22 22:57:51 -0500

    con

--- Conexion/conexion.py
+++ Conexion/conexion.py
@@ -1,13 +1,19 @@
 import mysql.connector
 
 def obtener_conexion():
-    conexion = mysql.connector.connect(
-        host='mysql-2d119ddc-uea-b69e.g.aivencloud.com',
-        user='avnadmin',
-        password='AVNS_XYJcQrtNUafY8HxHB-3',
-        port=23611,
-        database='defaultdb',
-        ssl_disabled=False,
-        ssl_verify_identity=False
-    )
-    return conexion
+    try:
+        conexion = mysql.connector.connect(
+            host='mysql-2d119ddc-uea-b69e.g.aivencloud.com',
+            user='avnadmin',
+            password='AVNS_XYJcQrtNUafY8HxHB-3',
+            port=23611,
+            database='defaultdb',
+            ssl_disabled=False,
+            ssl_verify_identity=False,
+            connect_timeout=10
+        )
+        return conexion
+    except Exception as e:
+        # Esto imprimirá el error exacto en la consola de Render
+        print(f"🚨 ERROR CRÍTICO DE BASE DE DATOS: {e}")
+        return None
