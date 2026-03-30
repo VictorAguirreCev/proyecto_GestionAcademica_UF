17de8f27c669c5a0f84a11dcab3fbf819d24b0a4
VictorAguirreCev <vb.aguirrec@uea.edu.ec>
2026-03-29 21:54:04 -0500

    Correccion critica: Ajuste de transaccion para Aiven (BINLOG_FORMAT)

--- Conexion/conexion.py
+++ Conexion/conexion.py
@@ -6,13 +6,19 @@ def obtener_conexion():
             host='mysql-2d119ddc-uea-b69e.g.aivencloud.com',
             user='avnadmin',
             password='AVNS_XYJcQrtNUafY8HxHB-3',
-            port=int(23611),        # Forzamos matemáticamente el número
+            port=int(23611),
             database='defaultdb',
             ssl_disabled=False,
             ssl_verify_identity=False,
-            use_pure=True           # SOLUCIÓN: Evita el colapso en Render
+            use_pure=True
         )
+        
+        # LA SOLUCIÓN PARA AIVEN: Ajustar el nivel de seguridad de la transacción
+        cursor = conexion.cursor()
+        cursor.execute("SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ")
+        cursor.close()
+        
         return conexion
     except Exception as e:
-        print(f"🚨 ERROR CRÍTICO DE BASE DE DATOS: {e}")
+        print(f"🚨 ERROR CRÍTICO DE CONEXIÓN: {e}")
         return None
